42181a2e50dbc5bce722c00499a7cc045b57e15a
Jason Clark <jasonclark602@yahoo.com>
2018-08-13 11:39:24 -0400

    Hi

--- database/schema.sql
+++ database/schema.sql
@@ -6,9 +6,12 @@ BEGIN;
 
 -- CREATE statements go here
 DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS house;
+DROP TABLE IF EXISTS user_team;
+DROP TABLE IF EXISTS team;
 
 CREATE TABLE app_user (
-  user_id SERIAL PRIMARY KEY,
+  user_id SERIAL,
   email varchar(100) NOT NULL,
   last_name varchar(32) NOT NUll,
   first_name varchar(32) NOT NULL,
@@ -16,7 +19,7 @@ CREATE TABLE app_user (
   password varchar(32) NOT NULL,
   role varchar(32) NOT NULL,
   salt varchar(255) NOT NULL,
-  team_name varchar(100)
+  CONSTRAINT pk_user_user_id PRIMARY KEY (user_id)
 );
 
 CREATE TABLE house(
@@ -25,18 +28,30 @@ CREATE TABLE house(
   resident varchar(64) NOT NULL,
   notes varchar(280),
   phone_number varchar(15),
-  status varchar(40) DEFAULT 'NV'
-  ADD CONSTRAINT check_status CHECK(status = 'NV' 
-  OR status = 'O' OR status = 'NI' OR status = 'FU' OR status = 'NS');  
+  status varchar(40) DEFAULT 'NV',
+  CONSTRAINT check_status CHECK(status = 'NV' 
+  OR status = 'O' OR status = 'NI' OR status = 'FU' OR status = 'NS')  
+  );
+  
+   CREATE TABLE team(
+    team_id SERIAL NOT NULL,
+    name varchar(64),
+    CONSTRAINT pk_team_id PRIMARY KEY (team_id)
+ );
+  
+  CREATE TABLE user_team(
+   user_id integer,
+   team_id integer,
+   CONSTRAINT pk_user_team_user_id_team_id PRIMARY KEY (user_id, team_id),
+   CONSTRAINT fk_user_team_user_id FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+   CONSTRAINT fk_user_team_team_id FOREIGN KEY (team_id) REFERENCES team(team_id)
+   
   );
   
 
- CREATE TABLE team(
-  team_id SERIAL,
-  admin_id integer PRIMARY KEY,
-  
+
  
- );
+ 
   
 
 COMMIT;
