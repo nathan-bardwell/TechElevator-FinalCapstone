7285ffac85d9032548f6e0e8df7d14da9893008c
Jason Clark <jasonclark602@yahoo.com>
2018-08-13 13:12:02 -0400

    Added new Stuff

--- src/main/java/com/techelevator/controller/HouseController.java
+++ src/main/java/com/techelevator/controller/HouseController.java
@@ -1,8 +1,26 @@
 package com.techelevator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.techelevator.model.HouseDAO;
 
 @Controller
 public class HouseController {
+	
+	private HouseDAO houseDAO;
+	
+	@Autowired
+	public HouseController(HouseDAO houseDAO) {
+		this.houseDAO = houseDAO;
+	}
+	
+	
+	@RequestMapping(path = "/addHouse",method = RequestMethod.GET)
+	public String addNewHouses() {
+		return null;
+	}
 
 }
--- src/main/java/com/techelevator/model/House.java
+++ src/main/java/com/techelevator/model/House.java
@@ -1,6 +1,53 @@
 package com.techelevator.model;
 
 public class House {
+	private long house_id;
+	private String address;
+	private String resident;
+	private String notes;
+	private String phoneNumber;
+	private String status;
+	
+	
+	
+	public long getHouse_id() {
+		return house_id;
+	}
+	public void setHouse_id(long house_id) {
+		this.house_id = house_id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getResident() {
+		return resident;
+	}
+	public void setResident(String resident) {
+		this.resident = resident;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 	
 
 }
