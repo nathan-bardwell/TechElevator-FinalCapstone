52b5741defc17cc01a7b3579873c74882ff3b9c5
Jason Clark <jasonclark602@yahoo.com>
2018-08-13 10:47:56 -0400

    updated database with new tables and columns

--- database/schema.sql
+++ database/schema.sql
@@ -8,11 +8,35 @@ BEGIN;
 DROP TABLE IF EXISTS app_user;
 
 CREATE TABLE app_user (
-  id SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
+  email varchar(100) NOT NULL,
+  last_name varchar(32) NOT NUll,
+  first_name varchar(32) NOT NULL,
   user_name varchar(32) NOT NULL UNIQUE,
   password varchar(32) NOT NULL,
-  role varchar(32),
-  salt varchar(255) NOT NULL
+  role varchar(32) NOT NULL,
+  salt varchar(255) NOT NULL,
+  team_name varchar(100)
 );
+
+CREATE TABLE house(
+  house_id SERIAL PRIMARY KEY,
+  address varchar(120) NOT NULL,
+  resident varchar(64) NOT NULL,
+  notes varchar(280),
+  phone_number varchar(15),
+  status varchar(40) DEFAULT 'NV'
+  ADD CONSTRAINT check_status CHECK(status = 'NV' 
+  OR status = 'O' OR status = 'NI' OR status = 'FU' OR status = 'NS');  
+  );
+  
+
+ CREATE TABLE team(
+  team_id SERIAL,
+  admin_id integer PRIMARY KEY,
+  
+ 
+ );
+  
 
 COMMIT;
