f43a916278929cd7168bc1067c5d51b2136e69dd
Nathan Bardwell <nathan.bardwell96@gmail.com>
2018-08-14 11:17:39 -0400

    Commit

--- src/main/java/com/techelevator/controller/HouseController.java
+++ src/main/java/com/techelevator/controller/HouseController.java
@@ -2,11 +2,13 @@ package com.techelevator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.techelevator.model.House;
 import com.techelevator.model.HouseDAO;
 
 @Controller
@@ -26,8 +28,8 @@ public class HouseController {
 	}
 	
 	@RequestMapping(path = "/addHouses",method = RequestMethod.POST)
-	public String addNewHouses(@RequestParam String address, @RequestParam String resident, @RequestParam String status, @RequestParam String phoneNumber, @RequestParam String notes) {
-		houseDAO.createHouse(address,resident,notes,phoneNumber,status);
+	public String addNewHouses(@ModelAttribute House house) {
+		houseDAO.createHouse(house.getAddress(), house.getResident(), house.getNotes(), house.getPhoneNumber(), house.getStatus());
 		return "redirect:/addHouses";
 	}
 	
