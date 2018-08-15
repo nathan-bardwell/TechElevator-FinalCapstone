cb9b35dd68ee67ff7ace13e443c73381b2280cc6
Steven Beard <etacalpha@gmail.com>
2018-08-15 10:38:54 -0400

    fixed method arguments

--- src/main/java/com/techelevator/model/HouseDAO.java
+++ src/main/java/com/techelevator/model/HouseDAO.java
@@ -3,11 +3,13 @@ package com.techelevator.model;
 import java.io.File;
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface HouseDAO {
 
 	public void createHouse(String address, String resident, String notes, String phone_number, String status);
 
-	public int createHouseByCsv(File file);
+	public int createHouseByCsv(MultipartFile file);
 	
 	public List<House> getHouseByTeam(long teamId);
 }
