901ed398852e2782f37f126c6e4d2982aaec5ba1
Nathan Bardwell <nathan.bardwell96@gmail.com>
2018-08-16 15:06:02 -0400

    Fixed my stupid mistake

--- src/main/java/com/techelevator/model/JDBCNoteDAO.java
+++ src/main/java/com/techelevator/model/JDBCNoteDAO.java
@@ -1,5 +1,56 @@
 package com.techelevator.model;
 
-public class JDBCNoteDAO {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JDBCNoteDAO implements NoteDAO{
+
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private NoteDAO noteDAO;
+	
+	@Override
+	public List<Note> getNotesByHouseId(Long houseId) {
+		List<Note> notes = new ArrayList<>();
+		String getNotesSql = "SELECT * "
+								+ "FROM note n"
+								+ "JOIN house_notes hn"
+								+ "ON n.note_id = hn.note_id "
+								+ "WHERE hn.house_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(getNotesSql, houseId);
+		while(results.next()) {
+			Note thisNote = new Note();
+			thisNote.setId(results.getLong("note_id"));
+			thisNote.setText(results.getString("text"));
+			thisNote.setTimestamp(results.getTimestamp("timestamp").toLocalDateTime());
+			
+			notes.add(thisNote);
+		}
+		return notes;
+	}
+
+	@Override
+	public void saveNewNote(Long houseId, String username, String text, LocalDateTime timestamp) {
+		String finalNote = text + "\n\n Submitted by: " + username;
+		Long noteId = null;
+		String newNoteSql = "INSERT INTO note (text, time) "
+							   + "VALUES (?, ?) RETURNING note_id";
+		SqlRowSet newNoteResult = jdbcTemplate.queryForRowSet(newNoteSql, finalNote, timestamp);
+		newNoteResult.next();
+		noteId = newNoteResult.getLong("note_id");
+		
+		String matchNoteToHouseSql = "INSERT INTO house_notes (house_id, note_id) "
+										  + "VALUES (?, ?)";
+		jdbcTemplate.update(matchNoteToHouseSql, houseId, noteId);
+		
+	}
 
 }
--- src/main/java/com/techelevator/model/Note.java
+++ src/main/java/com/techelevator/model/Note.java
@@ -1,5 +1,31 @@
 package com.techelevator.model;
 
+import java.time.LocalDateTime;
+
 public class Note {
+	
+	private Long id;
+	private String text;
+	private LocalDateTime timestamp;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
 
 }
